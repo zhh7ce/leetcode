92a73627b42a6a8cb5781dc2d14f16a58967ae77
ZHAO Hang <zhh7ce@qq.com>
2022-03-05 19:57:55 +0800

    521

---
+++ 521.最长特殊序列-ⅰ.py
@@ -0,0 +1,14 @@
+#
+# @lc app=leetcode.cn id=521 lang=python3
+#
+# [521] 最长特殊序列 Ⅰ
+#
+
+# @lc code=start
+class Solution:
+    def findLUSlength(self, a: str, b: str) -> int:
+        if a == b:
+            return -1
+        return max(len(a), len(b))
+# @lc code=end
+
